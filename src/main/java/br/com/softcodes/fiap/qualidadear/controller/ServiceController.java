99257f1ae685b99e57bb702cb57b03872e963a3e
kaoe <kaoe@sistemasprofissionais.com.br>
2024-10-18 12:15:48 -0300

    Atualização para uso de ENVs por ambiente

--- src/main/java/br/com/softcodes/fiap/qualidadear/controller/ServiceController.java
+++ src/main/java/br/com/softcodes/fiap/qualidadear/controller/ServiceController.java
@@ -1,5 +1,8 @@
 package br.com.softcodes.fiap.qualidadear.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/server")
 public class ServiceController {
 
+    @Value(value = "${SERVER_ENV}")
+    private String serverEnv;
+
+    enum Status {
+        OK,
+        ERROR
+    }
+
+    public record ResponseStatus(Status status, String env) {
+    }
+
     @RequestMapping("/status")
-    public String status() {
-        return "OK";
+    public ResponseEntity<ResponseStatus> status() {
+        return new ResponseEntity<>(new ResponseStatus(Status.OK, serverEnv), HttpStatus.OK);
     }
 
 }
